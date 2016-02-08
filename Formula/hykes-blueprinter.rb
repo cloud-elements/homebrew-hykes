429ea0f41057d84590407e6a74bf1ba78b9ac1fd
Rocky Madden <git@rockymadden.com>
2016-02-08 13:31:37 -0700

    hykes-blueprinter: v0.0.1

--- Formula/hykes-blueprinter.rb
+++ Formula/hykes-blueprinter.rb
@@ -1,17 +1,17 @@
 class HykesBlueprinter < Formula
   homepage "https://github.com/cloud-elements/hykes-blueprinter"
-  url "https://github.com/cloud-elements/hykes-blueprinter/archive/v0.0.0.tar.gz"
-  sha256 "995615dcdda202a6225def140803eae2703c0bdf2a56493430cce8ca411bbd7f"
+  url "https://github.com/cloud-elements/hykes-blueprinter/archive/v0.0.1.tar.gz"
+  sha256 "e21dd766f8b16564ef2d0de7535608b71306316c0a9196bc9c4ac539ac507295"
+
+  depends_on "ansible"
+  depends_on "coreutils"
+  depends_on "git"
 
   def install
     bin.mkpath
     (etc/"hykes-blueprinter").mkpath
     system "make", "install", "bindir=#{bin}", "etcdir=#{etc}/hykes-blueprinter"
   end
-
-  depends_on "ansible"
-  depends_on "coreutils"
-  depends_on "git"
 
   test do
     system "hykes-blueprinter", "--help"
