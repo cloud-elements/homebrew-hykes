2c268600f83c9460c3ead137df8b7265e1e22045
Rocky Madden <git@rockymadden.com>
2016-03-14 15:07:17 -0600

    hykes-provision: v0.2.1

--- Formula/hykes-provision.rb
+++ Formula/hykes-provision.rb
@@ -1,14 +1,13 @@
 class HykesProvision < Formula
   homepage "https://github.com/cloud-elements/hykes-provision"
-  url "https://github.com/cloud-elements/hykes-provision/archive/v0.2.0.tar.gz"
-  sha256 "1ac5b78895163f2f5568177ce382034cb847cb387ba80b9e1739200f6e389b65"
+  url "https://github.com/cloud-elements/hykes-provision/archive/v0.2.1.tar.gz"
+  sha256 "581f9c1f1e643a89634de8b987bdb5bcf607177118af1be0dd6f3eef7d833916"
 
   depends_on "coreutils"
   depends_on "hykes-blueprint"
 
   def install
     bin.mkpath
-    (etc/"hykes-provision").mkpath
     system "make", "install", "bindir=#{bin}"
   end
 
